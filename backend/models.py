82182ada2bf6c84a739a6381a11dac64a1ceb4f9
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-10 23:17:40 +0300

    add new class to models, userin and out

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,5 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field as PydanticField
 from sqlmodel import SQLModel, Field
-from sqlalchemy import Column, JSON
 from datetime import datetime
 
 
@@ -13,11 +12,29 @@ class User(SQLModel, table=True):
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
 
+class UserIn(BaseModel):
+    username: str = PydanticField(
+        min_length=3, max_length=32, pattern=r"^\w+$")
+    password: str = PydanticField(min_length=8, max_length=128)
+    email: EmailStr
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    username: str
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
 class Dream(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     category: str  # Example "tech" / "sport" / and if there not find your, so you can free text make it
     # When you click category, then you can just deep where you focus.
-    specific_items: list[str] = Field(default=[], sa_column=Column(JSON))
+    specific_items: list[str]
     is_active: bool = Field(default=True)
     created_at: datetime = Field(default_factory=datetime.utcnow)

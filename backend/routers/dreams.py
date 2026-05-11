3982b490145fa2e6143bc40fb33d20586e6da158
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-12 00:20:14 +0300

    add in dreams.py, specific-items, what save specific item to dream table in supabase

--- backend/routers/dreams.py
+++ backend/routers/dreams.py
@@ -1,3 +1,4 @@
+from sqlmodel import select
 from fastapi import APIRouter, HTTPException, Depends
 from backend.models import Dream
 from backend.routers.users import get_current_user
@@ -13,3 +14,16 @@ async def choose_category(category: str, session: Session = Depends(get_session)
     session.commit()
     session.refresh(new_dream)
     return new_dream
+
+
+@router.post("/specific-items", tags=["dream"], summary="You can make free text/choose after when you choosed category")
+async def specific_items(specific_items: list[str], session: Session = Depends(get_session), current_user=Depends(get_current_user)):
+    dream = session.exec(select(Dream).where(
+        Dream.user_id == current_user.id)).first()
+    if not dream:
+        raise HTTPException(status_code=404, detail="Dream not found")
+    dream.specific_items = specific_items
+    session.add(dream)
+    session.commit()
+    session.refresh(dream)
+    return dream

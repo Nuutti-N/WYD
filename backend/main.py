f2fc298f70e72c434b993308c8e3627196977b7c
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-11 17:03:35 +0300

    add in main.py, users router to main.py that will work in docs, and add in models.py more class that users.py work

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from backend.routers.users import router as users_router
 
 
 app = FastAPI()
+
+
+app.include_router(users_router)
--- backend/models.py
+++ backend/models.py
@@ -1,20 +1,21 @@
 from pydantic import BaseModel, EmailStr, Field as PydanticField
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, JSON
 from datetime import datetime
 
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     password: str
-    username: str = Field(unique=True)
+    username: str | None = None
     email: EmailStr = Field(unique=True)
     full_name: str | None = None
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
 
 class UserIn(BaseModel):
-    username: str = PydanticField(
-        min_length=3, max_length=32, pattern=r"^\w+$")
+    # full_name: str = PydanticField(
+    #     min_length=3, max_length=32, pattern=r"^\w+$")
     password: str = PydanticField(min_length=8, max_length=128)
     email: EmailStr
 
@@ -30,11 +31,25 @@ class Token(BaseModel):
     refresh_token: str
 
 
+class TokenPayload(BaseModel):
+    sub: str = None  # Username or Id
+    exp: int = None  # Expiration
+
+
+class SystemUser(BaseModel):
+    id: int
+    username: str
+
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
+
+
 class Dream(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     category: str  # Example "tech" / "sport" / and if there not find your, so you can free text make it
     # When you click category, then you can just deep where you focus.
-    specific_items: list[str]
+    specific_items: list[str] = Field(default=[], sa_column=Column(JSON))
     is_active: bool = Field(default=True)
     created_at: datetime = Field(default_factory=datetime.utcnow)

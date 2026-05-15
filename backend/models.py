33e227b767b8a4e603393b90392d16ebd7d3527c
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-15 16:15:10 +0300

    add xp_earned, what goinna check how much you have already xp

--- backend/models.py
+++ backend/models.py
@@ -70,6 +70,7 @@ class Checkin(SQLModel, table=True):
     note: str | None = None
     created_day: date = Field(default_factory=date.today)
     created_at: datetime = Field(default_factory=datetime.utcnow)
+    xp_earned: float = Field(default=None)
 
 
 class CheckinIn(BaseModel):

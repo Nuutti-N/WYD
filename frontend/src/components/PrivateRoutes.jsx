37b4e07faad55c574e689d8a896a3074d4c59105
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-16 20:26:59 +0300

    add localstorage stores jwt token, hashed pass stay in database

---
+++ frontend/src/components/PrivateRoutes.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom"
+
+
+function PrivateRoutes() {
+    const token = localStorage.getItem('token')
+
+    return (
+        token ? <Outlet /> : <Navigate to="/login" />
+
+    )
+
+}
+
+export default PrivateRoutes

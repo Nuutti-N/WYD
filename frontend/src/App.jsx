8e6a27d3c7f7f633f07a135a7967cc4c6ebcf705
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-19 21:57:58 +0300

    add in app.jsx dashboard route and add new dashboard file where is my homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import Login from "../pages/Login"
 import Onboarding from "../pages/Onboarding"
 import PrivateRoutes from "./components/PrivateRoutes"
 
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<PrivateRoutes />}>
           <Route path="/Onboarding" element={<Onboarding />} />
+          <Route path="Dashboard" element={<Dashboard />} />
         </Route>
         <Route path="/Register" element={<Register />} />
         <Route path="/Login" element={<Login />} />

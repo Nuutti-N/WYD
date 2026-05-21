94cdb6386e14ab828af1baf3621ccb132c23cd99
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-21 19:58:25 +0300

    add Logprogress route, that we can see in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "../pages/Login"
 import Onboarding from "../pages/Onboarding"
 import PrivateRoutes from "./components/PrivateRoutes"
 import Dashboard from "../pages/Dashboard"
+import Logprogress from "../pages/Logprogress"
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route element={<PrivateRoutes />}>
           <Route path="/Onboarding" element={<Onboarding />} />
-          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Logprogress" element={<Logprogress />} />
         </Route>
         <Route path="/Register" element={<Register />} />
         <Route path="/Login" element={<Login />} />

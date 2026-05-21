912630fa1c9e1f6a2d050f609c0c9058fdb6e39c
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-21 23:16:14 +0300

    add profile file, and app.jsx add route profile and make navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,19 @@ import Onboarding from "../pages/Onboarding"
 import PrivateRoutes from "./components/PrivateRoutes"
 import Dashboard from "../pages/Dashboard"
 import Logprogress from "../pages/Logprogress"
+import Profile from "../pages/Profile"
+import Navbar from "../pages/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route element={<PrivateRoutes />}>
           <Route path="/Onboarding" element={<Onboarding />} />
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Logprogress" element={<Logprogress />} />
+          <Route path="/Profile" element={<Profile />} />
         </Route>
         <Route path="/Register" element={<Register />} />
         <Route path="/Login" element={<Login />} />

bfd2217133f22d009142641427ba1ffdb93759bf
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-24 00:26:24 +0300

    add app url

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://wyd-sigma.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["GET", "DELETE", "POST", "PUT"],

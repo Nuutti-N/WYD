2d7915613baa388746c6293acd06b3139885dd32
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-06-03 00:27:53 +0300

    add state variables, and useEffect what fetch data

--- frontend/pages/Path.jsx
+++ frontend/pages/Path.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import api from "../api/client"
 
 
 function Paths() {
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(true)
+    const [paths, setPaths] = useState([])
+    const [activeTab, setActiveTab] = useState("Explore")
+    useEffect(() => {
+        setLoading(true)
+        async function fetchPaths() {
+            try {
+                const endpoint = activeTab === "Explore" ? "/paths" : "/paths/mine"
+                const response = await api.get(endpoint)
+                setPaths(response.data)
+            } catch (err) {
+                setError("No paths")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        fetchPaths()
+    }, [activeTab])
+
 
 }
 

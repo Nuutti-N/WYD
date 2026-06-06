981b3ae7c90a5451bb49a67b0b64472b746c1b5c
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-06-06 20:01:04 +0300

    add vite-plugim-pwa, and generate service worker for offline and installable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
@@ -6,5 +7,9 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: false,
+  })
   ],
 })

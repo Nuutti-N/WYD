a4c89038b2533644233846b86f9d9bd2cbb3c128
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-06-07 20:47:04 +0300

    add in requirements slowapi, and add rate-limit in main.py and own file, that make brain and working rate_limit in register and login

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from backend.routers.users import router as users_router
 from backend.routers.dreams import router as dreams
 from backend.routers.checkins import router as checkin
 from backend.routers.paths import router as paths
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi.errors import RateLimitExceeded
+from backend.rate_limiter import limiter
 
 app = FastAPI()
 
@@ -24,3 +27,13 @@ app.include_router(users_router)
 app.include_router(dreams)
 app.include_router(checkin)
 app.include_router(paths)
+
+app.state.limiter = limiter
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "Too many request. Please slow down and try again later."}
+    )
